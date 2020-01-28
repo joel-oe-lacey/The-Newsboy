a1e41157fd0964e608014883691c21b8c09a73f3
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-27 20:55:47 -0700

    Update button styling

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './NewsArticle.scss';
 
 const NewsArticle = (props) => {
-    return (<section className="news-article">
+    return (<article className="news-article">
         <img src={props.article.img} alt={props.article.headline}></img>
-        <h1>{props.article.headline}</h1>
-        <p>{props.article.description}</p>
-        <button><a href={props.article.url}>Link to the article</a></button>
-    </section>
+        <section className="news-content">
+            <h1>{props.article.headline}</h1>
+            <p>{props.article.description}</p>
+        </section>
+        <button><a href={props.article.url}>Link to the article  -></a></button>
+    </article>
     )}
 
 export default NewsArticle;
