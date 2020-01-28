e1e5b114d1ce85beeeb8192081004686822a1ad9
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-28 14:44:50 -0700

    Create Menu component with news type selection

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import './Menu.css'
+import './Menu.scss'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = (props) => {
+    const selectNews = (event, type) => {
+        event.preventDefault();
+        props.selectNewsType(type);
+    }
+
+    return (
+        <nav className="menu">
+            <h1>What's New?</h1>
+            <button onClick={(event) => selectNews(event, 'local')}>Local News</button>
+            <button onClick={(event) => selectNews(event, 'technology')}>Technology</button>
+            <button onClick={(event) => selectNews(event, 'entertainment')}>Entertainment</button>
+            <button onClick={(event) => selectNews(event, 'science')}>Science</button>
+            <button onClick={(event) => selectNews(event, 'health')}>Health</button>
+        </nav>
+    )
+}
 
 export default Menu;
