4bb8d465097e513c2a3fc032ebfbd5a858a4df89
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-29 18:35:44 -0700

    Add top level search function in app, and SearchForm component call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import science from '../../data/science';
 import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import SearchForm from '../SearchForm/SearchForm';
 import Menu from '../Menu/Menu';
 const allNews = {local, entertainment, health, science, technology}
 
@@ -22,9 +23,20 @@ class App extends Component {
     this.setState({articles: allNews[type]})
   }
 
+  searchCards = (searchTerm) => {
+    const casedSearch = searchTerm.toLowerCase();
+    const filteredArticles = this.state.articles.filter(article => {
+      return (article.headline.toLowerCase().includes(casedSearch) 
+        || article.description.toLowerCase().includes(casedSearch))
+      })
+
+    this.setState({articles: filteredArticles})
+  }
+
   render () {
     return (
       <div className="app">
+        <SearchForm searchCards={this.searchCards} />
         <Menu selectNewsType={this.selectNewsType} />
         <NewsContainer articles={this.state.articles} />
       </div>
